6e1b3c19a0666f74034f4f9b15a7f102feb2ed97
eymenkara <eymenkara@gmail.com>
2023-01-28 02:13:48 +0300

    Add files via upload
    Caesar Cipher program.

---
+++ caesar_cipher.py
@@ -0,0 +1,54 @@
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+
+def encrypt(plain_text, shift_amount):
+    crypt = ""
+    for i in plain_text:
+        crypt += alphabet[(alphabet.index(i) + shift_amount) % 26]
+    print(f"The encoded text is {crypt}")
+
+def decrypt(cipher_text, shift_amount):
+    original = ""
+    for i in cipher_text:
+        original += alphabet[(alphabet.index(i) - shift_amount) % 26]
+    print(f"The decoded text is {original}")
+
+def caesar(direction_inp, text_inp, shift_inp):
+    if direction_inp == "encode":
+        encrypt(plain_text=text_inp, shift_amount=shift_inp)
+    elif direction == "decode":
+        decrypt(cipher_text=text_inp, shift_amount=shift_inp)
+
+
+print(logo)
+cont = True
+while cont:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(direction, text, shift)
+    answer = input("Type 'yes' if you want to go again. Otherwise type 'no'. ")
+    if answer != "yes":
+        cont = False
+
